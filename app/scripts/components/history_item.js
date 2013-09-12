49a81af3f80238c26b23fc6a3ea2aeeb5b02d1f3
Sebastian Kippe <sebastian@kip.pe>
2013-09-12 18:48:25 +0200

    Improve history item rendering

--- app/scripts/components/history_item.js
+++ app/scripts/components/history_item.js
@@ -1,5 +1,11 @@
 Sharesome.HistoryItemComponent = Ember.Component.extend({
   isImage: function() {
     return this.get('url').match(/(jpg|jpeg|png|gif)$/);
+  }.property('url'),
+
+  itemStyle: function() {
+    if (this.get('isImage')) {
+      return 'background-image: url('+this.get('url')+')';
+    }
   }.property('url')
 });
