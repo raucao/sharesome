66a24870478e7961a6321c51b637b4b0298452cf
Sebastian Kippe <sebastian@kip.pe>
2013-09-12 17:08:55 +0200

    Drag n drop

--- app/scripts/views/upload_view.js
+++ app/scripts/views/upload_view.js
@@ -1,5 +1,23 @@
 Sharesome.UploadView = Ember.View.extend({
   templateName: 'upload',
   hasFileToUploadBinding: 'this.controller.fileToUpload',
-  classNameBindings: ['hasFileToUpload']
+  classNameBindings: ['hasFileToUpload'],
+
+  didInsertElement: function() {
+    this.$('#dropzone').bind('dragenter', function() {
+      $(this).addClass('drag-active');
+      return false;
+    });
+    this.$('#dropzone').bind('dragover', function (e) {
+      e.preventDefault();
+      return false;
+    });
+    this.$('#dropzone').bind('dragleave', function() {
+      $(this).removeClass('drag-active');
+      return false;
+    });
+    this.$('#dropzone').bind('drop', function(e) {
+      $(this).removeClass('drag-active');
+    });
+  }
 });
