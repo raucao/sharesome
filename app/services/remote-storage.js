5364afca1d5e9dec342a51f6914350397c233bd7
Sebastian Kippe <sebastian@kip.pe>
2016-01-24 20:32:28 -0500

    Bleh

--- app/services/remote-storage.js
+++ app/services/remote-storage.js
@@ -13,33 +13,45 @@ export default Ember.Service.extend({
 
     remoteStorage.on('ready', () => {
       console.log('RS ready');
-      this.set('connecting', false);
-      this.set('connected', true);
+      this.setProperties({
+        'connecting': false,
+        'connected': true
+      });
     });
     remoteStorage.on('not-connected', () => {
       console.log('RS not-connected');
-      this.set('connecting', false);
-      this.set('connected', false);
+      this.setProperties({
+        'connecting': false,
+        'connected': false
+      });
     });
     remoteStorage.on('connected', () => {
       console.log('RS connected');
-      this.set('connecting', false);
-      this.set('connected', true);
+      this.setProperties({
+        'connecting': false,
+        'connected': true
+      });
     });
     remoteStorage.on('disconnected', () => {
       console.log('RS disconnected');
-      this.set('connecting', false);
-      this.set('connected', false);
+      this.setProperties({
+        'connecting': false,
+        'connected': false
+      });
     });
     remoteStorage.on('connecting', () => {
       console.log('RS connecting');
-      this.set('connecting', true);
-      this.set('connected', false);
+      this.setProperties({
+        'connecting': true,
+        'connected': false
+      });
     });
     remoteStorage.on('authing', () => {
       console.log('RS authing');
-      this.set('connecting', true);
-      this.set('connected', false);
+      this.setProperties({
+        'connecting': true,
+        'connected': false
+      });
     });
   }.on('init')
 
