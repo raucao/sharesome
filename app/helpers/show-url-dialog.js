83a58aabcbf87e67cc3dc0b209535b91ce7da55c
Sebastian Kippe <sebastian@kip.pe>
2016-12-25 16:34:12 +0800

    Add icon buttons, copy-url button
    closes #48

--- app/helpers/show-url-dialog.js
+++ app/helpers/show-url-dialog.js
@@ -3,15 +3,17 @@ import { copyToClipboard } from 'sharesome/helpers/copy-to-clipboard';
 
 export function showUrlDialog(url) {
   window.vex.dialog.alert(
-    `Direct URL:<p><input type="text" value="${url}"> 
-     <button class="copy-url"><img src="" alt="copy">cc</button></p>`
+    `Direct URL:
+     <p><input type="text" value="${url}"></p>
+     <p class="actions"><button class="icon copy-url" title="copy to clipboard"><img src="/assets/images/copy.svg" alt="copy"></button></p>`
   );
   Ember.$('.vex-content input').first().select();
 
   Ember.$('.vex-content button.copy-url').on('click', (e) => {
     e.preventDefault();
     copyToClipboard(Ember.$('.vex-content input').val());
-    Ember.$('.vex-content button.copy-url').html('&#10003; done');
+    Ember.$('.vex-content button.copy-url img')
+         .attr('src', '/assets/images/checkmark.svg');
     setTimeout(() => {
       window.vex.closeAll();
     }, 1000);
