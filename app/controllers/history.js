149e1a93a84159dbaf1d3268639810c5f01f7ede
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-06-24 16:58:15 +0200

    Add search field
    Add search input filed to filter images by name in realtime

--- app/controllers/history.js
+++ app/controllers/history.js
@@ -3,8 +3,14 @@ import { sort } from '@ember/object/computed';
 
 export default Controller.extend({
 
+
   sortProperties: ['name:desc'],
   sortedModel: sort('model', 'sortProperties'),
+
+  queryChanged: function() {
+    this.set('filteredModel', this.get('model').filter(item => item.name.startsWith(this.get('q'))).sort((a, b) => a <= b ? 1 : -1));
+  }.observes('q', 'model'),
+  q: '',
 
   itemCount: function() {
     return this.get('model').length;
