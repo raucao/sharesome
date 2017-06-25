3d9411a810688a92c35ff16edc0dbba2e130a90e
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-06-25 16:12:11 +0200

    avoid using observer and use filteredModel to be computed as a property that depends on q and sortedModel

--- app/controllers/history.js
+++ app/controllers/history.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  queryChanged: function() {
-    this.set('filteredModel', this.get('model').filter( item => item.name.toLowerCase().includes(this.get('q'))).sort((a, b) => a <= b ? 1 : -1));
-  }.observes('q', 'model'),
+  sortProperties: ['name:desc'],
+  queryParams: ['q'],
   q: '',
-
+  sortedModel: Ember.computed.sort('model', 'sortProperties'),
+  filteredModel: Ember.computed.filter('sortedModel', function(item, index, array) {
+    return item.name.toLowerCase().includes(this.get('q'));
+  }).property('q'),
   itemCount: function() {
     return this.get('model').length;
   }.property('model.[]'),
