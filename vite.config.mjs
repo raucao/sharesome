9a92f9e828af095add7d7b03d9eafca996601d0c
Râu Cao <raucao@kosmos.org>
2026-06-15 13:57:24 +0200

    Re-instate prod build and scripts

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-export default defineConfig({
-  plugins: [
-    classicEmberSupport(),
-    ember(),
-    babel({
-      babelHelpers: 'runtime',
-      extensions,
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      outDir: mode === 'production' ? 'release' : 'dist',
+    },
+    plugins: [
+      classicEmberSupport(),
+      ember(),
+      babel({
+        babelHelpers: 'runtime',
+        extensions,
+      }),
+    ],
+  };
 });
